e620bfe39e7d50b81124b48d13092db8dc9fcb17
Abder-hbt <9907@holbertonstudents.com>
2024-10-21 21:13:40 +0000

    new

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+
+/*
+ *La fonction "int main(void)" est le point d'entrée du programme.
+ * Elle affiche un message et retourne 0 pour indiquer
+ * que le programme s'est terminé correctement.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
