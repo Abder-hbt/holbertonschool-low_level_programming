994e44235bc41fe8c1e350e92bd0d0225df449b0
Abder-hbt <9907@holbertonstudents.com>
2024-10-21 21:48:46 +0000

    new

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+*main - Entry point of the program
+*
+* Description: This function prints a welcome oh the programming
+* to the screen using the puts function. It returns 0
+* to indicate successful program termination.
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
