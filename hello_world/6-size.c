21c7d2691f8748bbb74818f24994d213e22f5d55
Abder-hbt <9907@holbertonstudents.com>
2024-10-22 09:21:32 +0000

    new

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@
 */
 int main(void)
 {
-printf("Size of char: %lu bytes\n", sizeof(char));
-printf("Size of int: %lu bytesn\n", sizeof(int));
-printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of long int: %lu bytes\n", sizeof(long int));
-printf("Size of long-long int: %lu bytes\n", sizeof(long long int));
+printf("Size of a char: %lu bytes\n", sizeof(char));
+printf("Size of a int: %lu bytesn\n", sizeof(int));
+printf("Size of a float: %lu bytes\n", sizeof(float));
+printf("Size of a long int: %lu bytes\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
 return (0);
 }
