64c5623ef533f715d4094269e9d2ce35709da883
Abder-hbt <9907@holbertonstudents.com>
2024-10-22 09:16:22 +0000

    new

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of char: %lu bytes\n", sizeof(char));
 printf("Size of int: %lu bytesn\n", sizeof(int));
 printf("Size of float: %lu bytes\n", sizeof(float));
 printf("Size of long int: %lu bytes\n", sizeof(long int));
-printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+printf("Size of long-long int: %lu bytes\n", sizeof(long long int));
 return (0);
 }
