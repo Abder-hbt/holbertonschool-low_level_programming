f7c0f56b5ccc881e2a928955800eba4ed959cff5
Abder-hbt <9907@holbertonstudents.com>
2024-10-22 14:43:38 +0000

    new

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-	puts("is zero");
+	printf("is zero\n");
 	}
 	else if (n > 0)
 	{
-	puts("is positive");
+	printf("is positive\n");
 	}
 	else
 	{
-	puts("is negative");
+	printf("is negative\n");
 	}
 	return (0);
 }
