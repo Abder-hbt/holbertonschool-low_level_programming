331e7b952aac771586041210247510b97549b10e
Abder-hbt <9907@holbertonstudents.com>
2024-10-22 08:36:06 +0000

    new

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+*main - Entry point of the program
+*
+* prints the size of various types on the computer
+* to the screen using the puts function. It returns 0
+* to indicate successful program termination.
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+int main(void)
+{
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of int: %lu bytesn\n", sizeof(int));
+printf("Size of float: %lu bytes\n", sizeof(float));
+printf("Size of double: %lu bytes\n", sizeof(double));
+printf("Size of pointer: %lu bytes\n", sizeof(void *));
+return (0);
+}
