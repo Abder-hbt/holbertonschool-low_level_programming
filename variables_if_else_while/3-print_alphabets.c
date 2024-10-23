baef260bbd9c5c519630107418dcf724cba39a20
Abder-hbt <9907@holbertonstudents.com>
2024-10-23 14:05:12 +0000

    new

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a' ; C >= 'Z' ; C++)
+		putchar(C);
+{
+	putchar('\n');
+}
+	return (0);
+}
