7747997c533525b8360463a58a27a32a3e7940e7
Abder-hbt <9907@holbertonstudents.com>
2024-10-23 12:11:09 +0000

    new

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, LD);
 	}
-	else if (LD < 6)
+	else if (LD < 6 && LD != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LD);
 	}
