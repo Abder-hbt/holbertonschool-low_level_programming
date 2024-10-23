418f5483ce214150b5a5d05883df1aae79549387
Abder-hbt <9907@holbertonstudents.com>
2024-10-23 18:01:36 +0000

    new

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+
+int main(void)
+{
+	{
+	char C;
+
+	for (C = 'z' ; C >= 'a' ; C--)
+		putchar(C);
+	}
+
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
