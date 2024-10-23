edb779c578f4530bc5087c1aa9b5550283cf6300
Abder-hbt <9907@holbertonstudents.com>
2024-10-23 18:10:40 +0000

    new

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+
+
+int main(void)
+
+{
+	char c = '0';
+	char h = 'A';
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (h = 'A'; h <= 'F'; h++)
+		putchar(h);
+	{
+		putchar('\n');
+	}
+	return (0);
+}
