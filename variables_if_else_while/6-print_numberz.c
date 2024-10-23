f04cfd2955feaad3c10bfdbe2da22647cb574260
Abder-hbt <9907@holbertonstudents.com>
2024-10-23 17:45:07 +0000

    new

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+
+
+int main(void)
+
+{
+	char c = '0';
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	{
+		putchar('\n');
+	}
+	return (0);
+}
