f2d5bee113ed402981b1fb4340f28ed17f4ae4aa
Abder-hbt <9907@holbertonstudents.com>
2024-10-27 19:25:06 +0000

    new

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/* more headers goes there */
+
+/**
+*print_alphabet_x10 - Entry point of the program
+*
+* Return: 0 if everything goes well, another value otherwise.
+*/
+
+/* betty styloc for function main goes there */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
