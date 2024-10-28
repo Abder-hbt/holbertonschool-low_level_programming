8941d3a862b32f72d95a471243c6614146b4c4d3
Abder-hbt <9907@holbertonstudents.com>
2024-10-28 15:31:38 +0000

    new

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Checks for uppercase character
+ * @a: The character to be checked
+ * @b: multi
+ * Return: a if c is uppercase, 0 otherwise.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
