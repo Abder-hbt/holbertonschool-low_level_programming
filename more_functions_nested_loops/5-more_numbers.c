f483b6b281a05338e998e4c867762a4683842fee
Abder-hbt <9907@holbertonstudents.com>
2024-10-29 10:34:21 +0000

    new

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - Affiche les chiffres de 0 à 9
+ *
+ * Return: Rien
+ */
+
+
+void more_numbers(void)
+
+{
+	int i;
+	int I;
+
+	for (I = 0; I < 10; I++)
+	{	
+	for (i = 1 ; i <= 14 ; i++)
+	{
+	if (i >= 10)
+	{
+	_putchar((i / 10) + '0');
+	}
+	_putchar((i % 10) + '0');
+	{
+	}
+	}
+	_putchar('\n');
+	}
+}
