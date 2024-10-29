deb60de1f2fee894ee4394633178b293fe7e221d
Abder-hbt <9907@holbertonstudents.com>
2024-10-29 16:19:28 +0000

    new

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- *
- * This program prints the numbers from 1 to 100, replacing multiples of 
- * three with "Fizz", multiples of five with "Buzz", and multiples of 
- * both with "FizzBuzz".
- *
  * Return: Always 0 (Success)
  */
 
@@ -16,17 +11,17 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
 		{
 		printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
 		printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-		printf("FizzBuzz ");
 		}
 		else
 		{
