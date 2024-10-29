b70a81e57c28858f4a8621dfaca41c9171d64625
Abder-hbt <9907@holbertonstudents.com>
2024-10-29 14:43:22 +0000

    new

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - Affiche les chiffres de 0 à 9
+ *
+ * @size : blablabla
+ *
+ * Return: Rien
+ */
+
+void print_square(int size)
+
+
+{
+	int i;
+	int r;
+
+		if (size < 0)
+	{
+		_putchar('\n');
+	}
+		else
+	{
+		for (i = 0; i < size; i++)
+	{
+		for (r = 0; r < size; r++)
+	{
+		_putchar(35);
+	}
+		_putchar('\n');
+	}
+	}
+}
