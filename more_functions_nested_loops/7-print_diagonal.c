d57a1eae891962a572a85e697ebc82827f5194c4
Abder-hbt <9907@holbertonstudents.com>
2024-10-29 14:12:28 +0000

    new

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_line - Affiche les chiffres de 0 à 9
+ *
+ * @n : blablabla
+ *
+ * Return: Rien
+ */
+
+void print_diagonal(int n)
+
+{
+	int i;
+	int e;
+		if (n < 0)
+	{
+		_putchar('\n');
+	}
+		else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (e = 0; e < i; e++)	
+	{
+		_putchar(32);
+
+	}
+		_putchar(92);
+		_putchar('\n');
+	}
+	}
+}
