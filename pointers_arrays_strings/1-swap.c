50e58689d05b514389ef703e9c1141c0e12fe888
Abder-hbt <9907@holbertonstudents.com>
2024-10-31 12:42:54 +0000

    new

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Affiche les chiffres de 0 à 9
+ *
+ * @a : blablabla
+ * @b : blablabla
+ * Return: Rien
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	*a = *b;
+	*b = *a;
+}
