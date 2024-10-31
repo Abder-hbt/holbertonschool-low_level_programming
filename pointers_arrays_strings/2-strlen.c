7c8deb1883a18bd9ec419b04be38035023a23f20
Abder-hbt <9907@holbertonstudents.com>
2024-10-31 14:40:27 +0000

    new

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reset_to_98 - Affiche les chiffres de 0 à 9
+ *
+ * @n : blablabla
+ *
+ * Return: Rien
+ */
+
+int _strlen(char *s)
+
+{
+	int L = 0;
+
+	while (s[L] != '\0')
+		L++;
+	{
+
+		return L;
+	}
+}
