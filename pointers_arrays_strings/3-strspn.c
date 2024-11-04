52a240f541f89c817a03dcbf47b5a409dff8cc8c
Abder-hbt <9907@holbertonstudents.com>
2024-11-04 23:10:05 +0000

    new

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,30 @@
 #include"main.h"
-
+/**
+ * _strspn - ecris le char b sur valeur pointer par s
+ *
+ * @accept : pointeur
+ *
+ * @s : nouvelle valeur
+ *
+ * Return: bytes
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
-	int index;
+	int i;
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[i])
 			{
 				bytes++;
 				break;
 			}
 
-			else if (accept[index + 1] == '\0')
+			else if (accept[i + 1] == '\0')
 				return (bytes);
 		}
 
