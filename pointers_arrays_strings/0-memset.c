5b3a50dbb674c99f97ec7650c9d0b02b15eb1fa2
Abder-hbt <9907@holbertonstudents.com>
2024-11-04 10:04:32 +0000

    new

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,15 @@
 #include"main.h"
+/**
+ * _memset - ecris le char b sur valeur pointer par s
+ *
+ * @s : pointeur
+ *
+ * @b : nouvelle valeur
+ *
+ * @n : placement de la veleur
+ *
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 
@@ -6,7 +17,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0 ; i <= n ; i++)
+	for (i = 0 ; i <= n ; i++)
 	{
 		s[i] = b;
 	}
