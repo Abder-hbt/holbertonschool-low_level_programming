77eafa9c4c43afa3d3874675c84ba529268d6cfc
Abder-hbt <9907@holbertonstudents.com>
2024-11-05 15:25:22 +0000

    new

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - ecris le char b sur valeur pointer par s
+ *
+ * @a : pointeur
+ *
+ * @size : blabla
+ *
+ * Return: NULL
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int S = 0;
+	unsigned int s = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		S += a[i * size + i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		s += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", S, s);
+}
