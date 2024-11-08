95ea13fd8ae7e1d6fc9370bc3a34a0424440848c
Abder-hbt <9907@holbertonstudents.com>
2024-11-08 10:19:23 +0000

    new

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * factorial - Returns the length of a string.
- * @n: The string to measure.
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root for.
  *
- * Return: The length of the string.
+ * * @x: The current number being tested as the square root.
+ *
+ * Return: The square root if it is a perfect square, or -1 if not.
  */
 
 int _sqrt_helper(int n, int x);
@@ -25,6 +27,15 @@ int _sqrt_recursion(int n)
 		return (_sqrt_helper(n, 0));
 	}
 }
+
+/**
+ * _sqrt_helper - Helper function to find the square root.
+ * @c: The number to calculate the square root for.
+ *
+ * @x: The current number being tested as the square root.
+ *
+ * Return: The square root if it is a perfect square, or -1 if not.
+ */
 
 int _sqrt_helper(int c, int x)
 
