28501eb54cd1687bb08c7c117375f22ef3213235
Abder-hbt <9907@holbertonstudents.com>
2024-11-14 10:32:38 +0000

    new

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+unsigned int i = 0; x = 0;
+
+{
+
+	if (s1 = NULL)
+	{
+	s1 = ("");
+	}
+
+	if (s2 = NULL)
+	{
+		s2 = ("");
+	}
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[x] != '\0')
+	{
+		x++;
+	}
