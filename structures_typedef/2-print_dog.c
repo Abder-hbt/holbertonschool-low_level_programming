1acd2425ab4a294125fa32c0177c4b2bcb10be6c
Abder-hbt <9907@holbertonstudents.com>
2024-11-15 15:23:55 +0000

    new

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"dog.h"
+
+void print_dog(struct dog *d)
+
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+
+	else
+	{
+		printf("name: %s\n", d->name);
+	}
+
+		printf("Age: %.6f\n", d->age);
+
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+		printf("Owner: %s\n", d->owner);
+}
