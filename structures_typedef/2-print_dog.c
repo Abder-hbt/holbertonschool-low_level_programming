061e6460617ff75588ed2c8c976f161ea0bbae38
Abder-hbt <9907@holbertonstudents.com>
2024-11-15 15:36:45 +0000

    new

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,28 +10,15 @@ void print_dog(struct dog *d)
 
 {
 	if (d == NULL)
-
 		return;
-
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-
-	else
-
-		printf("name: %s\n", d->name);
-
-	if (d->age < 0)
-
+		d->name = "(nil)";
+	if (d->age <= 0)
 		printf("Age: (nil)\n");
-
-	else
-
-		printf("Age: %f\n", d->age);
-
 	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-		printf("Owner: (nil)\n");
-
-	else
-		printf("Owner: %s\n", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
