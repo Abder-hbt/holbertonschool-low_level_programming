c5de70719911178bd4de66489603e1d52138c380
Abder-hbt <9907@holbertonstudents.com>
2024-11-19 22:33:29 +0000

    new

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -6,6 +6,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+/**
+ * struct op - Structure to map operator strings to corresponding functions.
+ * @op: The operator as a string.
+ * @f: Pointer to the function that performs the operation.
+ */
 typedef struct op
 
 {
@@ -13,6 +19,7 @@ typedef struct op
 	int (*f)(int, int);
 } op_t;
 
+/* Function prototypes for arithmetic operations and function selector */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -5,22 +5,22 @@
 
 /**
  * op_add - Adds two integers.
- * @a: First integer.
- * @b: Second integer.
- * 
+ * @num1: First integer.
+ * @num2: Second integer.
+ *
  * Return: The sum of a and b.
  */
 int op_add(int num1, int num2)
 
 {
-	return ( num1 + num2);
+	return (num1 + num2);
 }
 
 /**
  * op_sub - Subtracts two integers.
- * @a: First integer.
- * @b: Second integer.
- * 
+ * @num1: First integer.
+ * @num2: Second integer.
+ *
  * Return: The difference of a and b.
  */
 int op_sub(int num1, int num2)
@@ -31,8 +31,8 @@ int op_sub(int num1, int num2)
 
 /**
  * op_mul - Multiplies two integers.
- * @a: First integer.
- * @b: Second integer.
+ * @num1: First integer.
+ * @num2: Second integer.
  *
  * Return: The product of a and b.
  */
@@ -44,9 +44,9 @@ int op_mul(int num1, int num2)
 
 /**
  * op_div - Divides two integers.
- * @a: First integer.
- * @b: Second integer.
- * 
+ * @num1: First integer.
+ * @num2: Second integer.
+ *
  * Return: The result of the division of a by b.
  *         If b is 0, exits the program with status 100.
  */
@@ -62,8 +62,8 @@ int op_div(int num1, int num2)
 
 /**
  * op_mod - Computes the modulus of two integers.
- * @a: First integer.
- * @b: Second integer.
+ * @num1: First integer.
+ * @num2: Second integer.
  *
  * Return: The remainder of the division of a by b.
  *         If b is 0, exits the program with status 100.
