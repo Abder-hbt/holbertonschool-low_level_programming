8c21fa99fa971608ea307fb298dfbfe8d471756a
Abder-hbt <9907@holbertonstudents.com>
2024-11-19 13:36:55 +0000

    new

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,7 +6,7 @@
  *
  * Return: The index of the first matching element,
  *         -1 if no match is found or if size <= 0.
- */
+ */ 
 
 #include"function_pointers.h"
 #include <stddef.h>
@@ -20,6 +20,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
+	if (array != NULL && cmp != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 		(*cmp)(array[i]);
@@ -28,6 +30,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 		return (i);
 		}
+	}
 	}
 		return (-1);
 }
