0993988d5d2e2a6468eeeb453f720cb8a8911cf2
Abder-hbt <9907@holbertonstudents.com>
2024-12-08 16:25:30 +0000

    new

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,16 +12,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new;
 
-if (head == NULL)
-return (NULL);
-
 new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-new->n = n;
-new->next = *head;
-*head = new;
+	new->n = n;
+	new->next = *head;
 
-return (new);
+	*head = new;
+
+	return (new);
 }
